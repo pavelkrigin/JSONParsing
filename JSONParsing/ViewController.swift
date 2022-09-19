d087b57269ec04a226ae8ad8f7a3fbd543dc2f84
Pavel Krigin <pavel.krigin.85@gmail.com>
2022-09-19 08:12:31 +0300

    Initial Commit

---
+++ JSONParsing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSONParsing
+//
+//  Created by Pavel Krigin on 19.9.22..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
